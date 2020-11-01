59645e1d8fd8a6b8afc35a402a9b1361931f03cb
Supratim Mondal <supratimmondal50@gmail.com>
2020-11-01 20:36:37 +0530

    Connect to fron-end using peer

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
-const { Socket } = require("dgram");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const PORT = process.env.PORT || 4000;
-
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
+// creating the peer server
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, { debug: true });
+
+const PORT = process.env.PORT || 4000;
 
 app.use(cors());
+app.use("/peerjs", peerServer); // connect peer using middleware
 
 io.on("connection", (socket) => {
-  socket.on("newUserJoin", ({ userName, imageUrl }) => {
-    console.log(`New User Join. Name is ${userName}image is ${imageUrl}`);
+  socket.on("newUserJoin", ({ name, imageUrl }, id) => {
+    console.log(
+      `New User Join. Name is ${name} image is ${imageUrl} and Id is ${id}`
+    );
   });
 });
 
