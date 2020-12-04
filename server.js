d581206ea0050edf8e8423ce62a9311d5af77419
Supratim Mondal <supratimmondal50@gmail.com>
2020-12-04 17:42:26 +0530

    Re-create server

--- server.js
+++ server.js
@@ -1,29 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const server = require("http").Server(app);
-const io = require("socket.io")(server);
-// creating the peer server
-const { ExpressPeerServer } = require("peer");
-const peerServer = ExpressPeerServer(server, { debug: true });
+const http = require("http");
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-const PORT = process.env.PORT || 4000;
-
-app.use(cors());
-app.use("/peerjs", peerServer); // connect peer using middleware
+const port = process.env.PORT || 5000;
 
 io.on("connection", (socket) => {
-  socket.on("newUserJoin", ({ name, imageUrl }, id) => {
-    console.log(
-      `New User Join. Name is ${name} image is ${imageUrl} and Id is ${id}`
-    );
+  socket.on("join user", ({ roomID, roomName }) => {
+    console.log(`room name ${roomName} room id ${roomID}`);
   });
 });
 
-app.get("/", (req, res) => {
-  res.send("This is Server");
-});
-
-server.listen(PORT, () => {
-  console.log(`Server up and running on ${PORT}`);
+server.listen(port, () => {
+  console.log(`Server up and running on port number ${port}`);
 });
